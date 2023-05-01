3945b86d2350ff1f148cfad24d87e59298d21af7
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-01 16:11:39 +0100

    some changes

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,12 +1,21 @@
 export default function Page(){
     return(
         <>
-            <div className="flex-col select-none space-y-1 font-[YesevaOne] font-semibold tracking-wider text-white text-9xl">
-                <h1 className="pl-32">World</h1>
-                <h1 className="pl-10 flex -space-x-[182px] h-">Cl
-                    <span className=" text-transparent stroke">ass</span>
-                    <span className="text-white opacity-70">ass</span>
-                </h1>
+            <div className="flex-col w-full select-none -space-y- font-[YesevaOne] font-semibold tracking-wider text-white text-9xl">
+                <div className="grid grid-rows-2 grid-flow-col w-full border-2">
+                    <h1 className="pl-32 mr-auto border-2">World</h1>
+                    <h1 className="pl-10 flex -space-x-[182px] mr-auto border-2 h-">Cl
+                        <span className=" text-transparent stroke">ass</span>
+                        <span className="text-white opacity-70">ass</span>
+                    </h1>
+
+                    <div className=" p-10 font-[Mulish] tracking-normal">
+                        <h1 className="text-sm font-medium">Available premium-quality men’s and<br/> women’s perfumes, trusted by well-<br/>known brands, with proven quality,<br/>refresh your day</h1>
+                        <div>
+                            <h1 className="text-xl cursor-pointer text-black bg-[#E0EBFF] w-fit py-2 px-10 rounded-t-3xl rounded-b-3xl ">Explore</h1>
+                        </div>
+                    </div>
+                </div>
                 <h1 className="pl-52">fragrance</h1>
             </div>
         </>
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,7 +1,7 @@
 export default function Socials(){
     return(
         <>
-            <div className="pl-12 py-[10%] w-24 my-12 border-r-[1px] text-sm font-[Mulish] font-thin text-white">
+            <div className="pl-12 py-[10%] w-24 my-12 border-[#3D475E] border-r-[2px] text-sm font-[Mulish] font-thin text-white">
                 <div className="rotate-180" style={{writingMode: 'vertical-rl'}}><a className="" href="#">Instagram</a></div>
                 <div className="py-12 rotate-180" style={{writingMode: 'vertical-rl'}}><a className="" href="#">Twitter</a></div>
                 <div className="rotate-180" style={{writingMode: 'vertical-rl'}} ><a className="" href="#">Facebook</a></div>
